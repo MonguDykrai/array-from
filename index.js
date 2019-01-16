eb6af768752ca0cd7d2d242d83159b4cc71921d1
jicf <jicf@zylliondata.com>
2019-01-16 10:38:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// module.exports = function ArrayFrom(arrayLike) {
+//   return Array.from(arrayLike)
+// }
+
+export default function ArrayFrom(arrayLike) {
+  return Array.from(arrayLike)
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+const ArrayFrom = require('./index')
+
+console.log(ArrayFrom)
