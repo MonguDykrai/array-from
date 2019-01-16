75e15556196f231dd1fb4ec667c90e86ea862bb9
文句大嫌 <40307989+MonguDykrai@users.noreply.github.com>
2019-01-16 10:42:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// module.exports = function ArrayFrom(arrayLike) {
-//   return Array.from(arrayLike)
-// }
-
-export default function ArrayFrom(arrayLike) {
+module.exports = function ArrayFrom(arrayLike) {
   return Array.from(arrayLike)
 }
+
+// export default function ArrayFrom(arrayLike) {
+//   return Array.from(arrayLike)
+// }
